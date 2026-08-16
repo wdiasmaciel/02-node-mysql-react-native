47189d51b6ea6a05aa0cd87918b30b30b52e76ee
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-16 07:21:06 +0000

    Exemplo

--- bd.js
+++ bd.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
 
 const bd = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password: 'root',
     database: 'livraria_db'
